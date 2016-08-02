d650628aa6a71bdacb23307db07aae7ccf0449c0
Santiago Martinez <martinez_santi98@hotmail.com>
2016-08-02 19:40:53 -0300

    El error es tuyo

--- utils.rb
+++ utils.rb
@@ -1,9 +1,5 @@
 module Utils
   def self.say_hello_to(name, language)
-    if language == "es"
-      puts "Hola #{name}!"
-    else
-      puts "No entiendo tu idioma"
-    end
+    language == "es" ? puts "Hola #{name}!" : puts "No comprendo tu idioma"
   end
 end
