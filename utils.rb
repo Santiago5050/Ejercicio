c6a444416c168ca6ed774cc1faf98d9d98f5cde3
Alvaro <alvarogaston1997@hotmail.com>
2016-08-02 18:40:54 -0400

    Futuro error

--- utils.rb
+++ utils.rb
@@ -1,9 +1,9 @@
 module Utils
   def self.say_hello_to(name, language)
-    if language == "es"
+    unless language != "es"
       puts "Hola #{name}!"
     else
-      puts "No entiendo tu idioma"
+      puts "No hablo tu idioma"
     end
   end
 end
